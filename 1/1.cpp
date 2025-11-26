6c410dd7f9f3332e464f77b383b975d28817e415
malyu <malyu@M414ND-LAPTOP>
2025-11-26 23:16:16 +0300

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+
+    int sum = a + b;
+
+    cout << "Сумма = " << sum << endl;
+
+    return 0;
+}
